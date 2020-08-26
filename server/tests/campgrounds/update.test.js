68eb024cfd2ea9972d09299f9e5342478acaa299
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2020-08-26 16:44:58 +0200

    added tests for campground update routes :alembic:

---
+++ server/tests/campgrounds/update.test.js
@@ -0,0 +1,173 @@
+/**
+ * * Modules
+ * * Imports
+ */
+const app = require('../../src/app');
+const request = require('supertest');
+const {
+  setupDatabase,
+  userOneToken,
+  userTwoToken,
+  campgroundOneId,
+  Campground,
+} = require('../fixtures/db');
+
+beforeEach(setupDatabase);
+
+/**
+ * * Tests
+ * * Authorization
+ */
+test("Should not update with other user's token", async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userTwoToken}`])
+    .send()
+    .expect(404);
+});
+
+/**
+ * * Tests
+ * * Title Field
+ */
+test('Should not update title with short title', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      title: 'aaa',
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.title).not.toBe('aaa');
+});
+
+test('Should not update title with long title', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      title: 'a'.repeat(21),
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.title).not.toBe('a'.repeat(21));
+});
+
+test('Should not update title with long title', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      title: 'a'.repeat(21),
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.title).not.toBe('a'.repeat(21));
+});
+
+test('Should not update title with non alphanumeric title', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      title: 'Invalid $/',
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.title).not.toBe('Invalid $/');
+});
+
+/**
+ * * Tests
+ * * Image Field
+ */
+test('Should not update image with invalid image URL', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      image: 'invalid',
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.image).not.toBe('invalid');
+});
+
+/**
+ * * Tests
+ * * Description Field
+ */
+test('Should not update description with short description', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      image: 'a'.repeat(10),
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.description).not.toBe('a'.repeat(10));
+});
+
+test('Should not update description with long description', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      image: 'a'.repeat(2501),
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.description).not.toBe('a'.repeat(2501));
+});
+
+/**
+ * * Tests
+ * * Price Field
+ */
+test('Should not update price with invalid price', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      price: 'abc',
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.price).not.toBe('abc');
+});
+
+test('Should not update price with low price', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      price: '0.9',
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.price).not.toBe(0.9);
+});
+
+test('Should not update price with high price', async () => {
+  await request(app)
+    .patch(`/campgrounds/${campgroundOneId}`)
+    .set('Cookie', [`auth_token=${userOneToken}`])
+    .send({
+      price: 1002,
+    })
+    .expect(400);
+
+  const campground = await Campground.findOne({ _id: campgroundOneId });
+  expect(campground.price).not.toBe(1002);
+});
